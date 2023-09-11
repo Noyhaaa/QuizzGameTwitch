5ea4ba7aed18f44c227fe1602881f02a2c2ab590
Noyhaaa <yohansti@outlook.fr>
2023-09-11 22:04:03 +0200

    Golang game server with javascript/html/css part
    - Create server with Get method for redirection
    - Create home/room/game pages
    - Implement some JS to show what answer is selected

--- main.go
+++ main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
+
 	"github.com/Noyhaaa/QuizzGameTwitch/src/pkg/modules/dbutils"
+	"github.com/Noyhaaa/QuizzGameTwitch/src/pkg/modules/quizzapi"
 )
 
 func main(){
 	dbutils.InitDB()
+	router := gin.Default()
+	router.LoadHTMLGlob("src/web/templates/*")
+	router.Static("/static/css", "src/web/static/css")
+
+	router.GET("/", quizzapi.HomePage)
+
+	router.GET("/myroom", quizzapi.MyRoomPage)
+
+	router.GET("/game", quizzapi.StartGame)
+
+	// Start web server on port 8080
+    router.Run(":8080")
 	
 	fmt.Println(dbutils.CurrentQuestionInfo())
 	//create new question info
---
+++ src/pkg/modules/quizzapi/quizzapi.go
@@ -0,0 +1,41 @@
+package quizzapi
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/gin-gonic/gin"
+
+	"github.com/Noyhaaa/QuizzGameTwitch/src/pkg/modules/dbutils"
+)
+
+func HomePage(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"message": "Bienvenue dans le JiJi Quizz",
+	})
+}
+
+func MyRoomPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "myroom.html", gin.H{
+		"message": "Bienvenue dans la room de JiJi",
+	})
+}
+
+func StartGame(c *gin.Context) {
+	var answers []string
+	questionInfo := dbutils.CurrentQuestionInfo()
+
+	if _, ok := questionInfo["answers"].([]string); ok{ 
+		answers = questionInfo["answers"].([]string)
+	} else {
+		fmt.Println("Can't retrieve the answers of the question")
+	}
+
+	c.HTML(http.StatusOK, "game.html", gin.H{
+		"question": questionInfo["question"],
+		"answer1": answers[0],
+		"answer2": answers[1],
+		"answer3": answers[2],
+		"answer4": answers[3],
+		"correctAnswer": questionInfo["correct"],
+	})
+}
